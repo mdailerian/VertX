1048c9aae503fdc0987a8b78bc41ac99b5acbac0
mdailerian <martin@dailerian.com>
2016-05-02 04:44:09 -0400

    working version

--- src/VertXRunners/EmbeddedVertx.java
+++ src/VertXRunners/EmbeddedVertx.java
@@ -2,12 +2,12 @@ package VertXRunners;
 
 import io.vertx.core.Vertx;
 
-https://github.com/vert-x3/vertx-examples/blob/master/core-examples/src/main/java/io/vertx/example/core/embed/EmbeddedServer.java
+//https://github.com/vert-x3/vertx-examples/blob/master/core-examples/src/main/java/io/vertx/example/core/embed/EmbeddedServer.java
 
 /*
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class EmbeddedServer {
+public class EmbeddedVertx {
 
     public static void main(String[] args) {
         // Create an HTTP server which simply returns "Hello World!" to each request.
